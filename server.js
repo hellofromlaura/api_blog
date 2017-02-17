4284b9a7f8afc521d5c71f57e918ae47b415d020
Laura Wolfart <laurawolfart@gmail.com>
2017-02-17 15:50:36 -0500

    finished project

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/blog-posts', (req, res) => {
 
 
 
-app.post('/blog-post', jsonParser, (req, res) => {
+app.post('/blog-posts', jsonParser, (req, res) => {
   // ensure `name` and `budget` are in request body
   const requiredFields = ['title', 'content', 'author'];
   for (let i=0; i<requiredFields.length; i++) {
@@ -33,23 +33,46 @@ app.post('/blog-post', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-const item = BlogPosts.create(
-    {req.body.title, req.body.content, req.body.author});
+const item = BlogPosts.create
+  (req.body.title, req.body.content, req.body.author);
   res.status(201).json(item);
 });
 
+app.delete('/blog-posts/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post id \`${req.params.id}\``);
+  res.status(204).end();
+});
 
-
-
-
-
-
-
-
-
-
-
-
+app.put('/blog-posts/:id', jsonParser, (req, res) => {
+	console.log(req.params);
+	console.log(req.body);
+  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(200).json(updatedItem);
+});
 
 
 
